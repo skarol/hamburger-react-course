b5e0e3a78a4f6eebfbb74a41b0029e6f3f8b4e90
Karolina Samorek <karolina.samorek@gmail.com>
2019-07-23 12:42:52 +0200

    Before hamburger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -39,33 +40,27 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let buttonClass = '';
+
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangeHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangeHandler(event, person.id)}
+                />
+              </ ErrorBoundary>
             );
           })}
         </div>
       );
-      style.backgroundColor = 'red';
+      buttonClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -80,7 +75,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <h1 className={assignedClasses.join(' ')}>Hi, I'm a React App</h1>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <button className={buttonClass} onClick={this.togglePersonsHandler}>
           Toggle persons
         </button>
         {persons}

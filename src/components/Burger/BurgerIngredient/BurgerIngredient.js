ea328cacc1cb1e4c4145e776bca5f66192230599
Karolina Samorek <karolina.samorek@gmail.com>
2019-07-23 14:59:40 +0200

    Add prop-types

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
@@ -29,9 +30,16 @@ const burgerIngredient = props => {
     case 'bacon':
       ingredient = <div className={classes.Bacon} />;
       break;
+
+    default:
+      ingredient = null;
   }
 
   return ingredient;
+};
+
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
 };
 
 export default burgerIngredient;
